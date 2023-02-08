4db57d7e192bf95d378879e35b04a2b409eb4e97
Cody Swartz <ctsstc@gmail.com>
2023-02-08 03:01:53 -0800

    Add registration page with form
    This provides a page with a form with a field for an email, password, and password verification field, and a Register button to go along with it to submit the form.

---
+++ pocketbase-svelete-kit-auth-tailwind-daisy-ui/src/routes/register/+page.svelte
@@ -0,0 +1,21 @@
+<form method="POST" class="card">
+	<h1 class="h1 text-2xl mb-8">Register</h1>
+	<div class="form-control gap-2 mb-4">
+		<input type="email" name="email" placeholder="Email" id="" class="input input-bordered" />
+		<input
+			type="password"
+			name="password"
+			placeholder="Password"
+			id=""
+			class="input input-bordered"
+		/>
+		<input
+			type="password"
+			name="passwordConfirm"
+			placeholder="Confirm Password"
+			id=""
+			class="input input-bordered"
+		/>
+		<button class="btn btn-primary">Register</button>
+	</div>
+</form>

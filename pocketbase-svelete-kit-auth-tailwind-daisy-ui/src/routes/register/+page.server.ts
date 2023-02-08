0e66c3ba9c5da1c1f1ea46085105b30d2981f073
Cody Swartz <ctsstc@gmail.com>
2023-02-08 03:29:04 -0800

    Add server registration handler
    This will create the user in pocketbase from the registration form data, and then redirect them to the home page.

---
+++ pocketbase-svelete-kit-auth-tailwind-daisy-ui/src/routes/register/+page.server.ts
@@ -0,0 +1,23 @@
+import { redirect, type Actions } from '@sveltejs/kit';
+
+export const actions: Actions = {
+	default: async ({ locals, request }) => {
+		const data = Object.fromEntries(await request.formData()) as {
+			email: string;
+			password: string;
+			passwordConfirm: string;
+		};
+
+		try {
+			const pbUsers = locals.pb.collection('users');
+
+			await pbUsers.create(data);
+			await pbUsers.authWithPassword(data.email, data.password);
+		} catch (e) {
+			console.error(e);
+			throw e;
+		}
+
+		throw redirect(303, '/');
+	}
+};

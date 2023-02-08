2211b4de18e8e8a504a9e924e5f928535b182dda
Cody Swartz <ctsstc@gmail.com>
2023-02-08 03:42:39 -0800

    Utilize form enhancement on the registration page
    This utilizes progressive enhancement which allows us to hook into the submission and it upgrades it into ajax iirc along with other magic explained here:
    
    https://kit.svelte.dev/docs/form-actions#progressive-enhancement

--- pocketbase-svelete-kit-auth-tailwind-daisy-ui/src/routes/register/+page.svelte
+++ pocketbase-svelete-kit-auth-tailwind-daisy-ui/src/routes/register/+page.svelte
@@ -1,4 +1,18 @@
-<form method="POST" class="card">
+<script lang="ts">
+	import { applyAction, enhance } from '$app/forms';
+	import { pb } from '$lib/pocketbase';
+</script>
+
+<form
+	method="POST"
+	class="card"
+	use:enhance={() => {
+		return async ({ result }) => {
+			pb.authStore.loadFromCookie(document.cookie);
+			await applyAction(result);
+		};
+	}}
+>
 	<h1 class="h1 text-2xl mb-8">Register</h1>
 	<div class="form-control gap-2 mb-4">
 		<input type="email" name="email" placeholder="Email" class="input input-bordered" />

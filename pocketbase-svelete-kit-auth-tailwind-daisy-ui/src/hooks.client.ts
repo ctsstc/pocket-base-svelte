0446de874b7e9a7b7435a48ffb17f3540389c280
Cody Swartz <ctsstc@gmail.com>
2023-02-08 02:49:21 -0800

    Add client auth hooks to set current user
    This loads the pocketbase auth store from cookies, and then sets the svelte currentUser store to the user from pocketbase's auth store.
    
    If the pocketbase auth store changes we update our cookie to the latest pocketbase auth store.

---
+++ pocketbase-svelete-kit-auth-tailwind-daisy-ui/src/hooks.client.ts
@@ -0,0 +1,10 @@
+import { currentUser, pb } from '$lib/pocketbase';
+
+pb.authStore.loadFromCookie(document.cookie);
+
+pb.authStore.onChange(() => {
+  currentUser.set(pb.authStore.model);
+  document.cookie = pb.authStore.exportToCookie({
+    httpOnly: false,
+  });
+});
